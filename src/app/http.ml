a0c9b0ab66c7ff483448f241fc709b8336206fd6
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2012-08-20 17:53:47 +0200

    Http.get: force refreshing the cache every day.

--- src/app/http.ml
+++ src/app/http.ml
@@ -5,11 +5,17 @@
 open Printf
 open Http_client.Convenience
 
+let not_too_old fn =
+  let now = Unix.time () in (* in sec *)
+  let modif = (Unix.stat fn).Unix.st_mtime in
+  eprintf "*** Now: %f  %S: %f\n%!" now fn modif;
+  now <= modif +. 86400. (* 60 * 60 * 24 = 24h *)
+
 let get url =
   let md5 = Digest.to_hex(Digest.string url) in
   let fn = Filename.concat Filename.temp_dir_name ("ocamlorg-" ^ md5) in
   eprintf "Downloading %s... %!" url;
-  if Sys.file_exists fn then (
+  if Sys.file_exists fn && not_too_old fn then (
     eprintf "done.\n  (using cache %s).\n%!" fn;
     let fh = open_in fn in
     let data = input_value fh in
