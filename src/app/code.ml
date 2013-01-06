22a45821492241206fab919176c617ceeb6436df
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2013-01-06 06:59:37 +0100

    Syntax highlight optional arguments of functions.

--- src/app/code.ml
+++ src/app/code.ml
@@ -15,7 +15,7 @@ let highlight_ocaml =
   let let_id = id ^ "\\|( +[!=+-*/^:]+ +)" in
   let uid = "\\b[A-Z][A-Za-z0-9_']*" in
   (* Arguments to functions may pattern match. *)
-  let args = "[^=<> ][^=<>]*" in
+  let args = "\\(\\?([^=()]+=[^=()]+) *\\)+\\|[^=<>? ][^=<>?]*" in
   let subst = [ (* regex, replacement *)
     (let cmt_txt = "\\([^()]\\|([^*][^()]*[^*])\\)*" in
      "\\((\\*\\((\\*" ^ cmt_txt ^ "\\*)\\|" ^ cmt_txt ^ "\\)+\\*)\\)",
