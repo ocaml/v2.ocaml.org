5e712b9cd6e70da323d55b4990afa6cdc30bd92c
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2014-02-28 00:03:30 +0100

    Add <link rel="alternate" hreflang> to indicate translations
    See https://support.google.com/webmasters/answer/189077?hl=en for more
    information

--- script/translations.ml
+++ script/translations.ml
@@ -18,21 +18,43 @@ let translations path =
                 let fhtml = if l = "" then
                               if p = "index" then "." else p ^ ".html"
                             else p ^ "." ^ l ^ ".html" in
-                t := (l, fhtml) :: !t
+                t := ((if l = "" then "en" else l), fhtml, l = l0) :: !t
              ) d;
-  let t = List.sort (fun (l1,_) (l2,_) -> String.compare l1 l2) !t in
-  (* Convert to HTML *)
-  let to_html (l, fn) =
-    if l = l0 then sprintf "<li class=\"active\">%s</li>\n" (lang_to_html l)
-    else sprintf "<li><a href=\"%s\">%s</a></li>\n" fn (lang_to_html l) in
-  String.concat "" (List.map to_html t)
+  List.sort (fun (l1,_,_) (l2,_,_) -> String.compare l1 l2) !t
+
+(* Convert to HTML *)
+let print_html t =
+  let to_html (l, fn, current) =
+    if current then printf "<li class=\"active\">%s</li>\n" (lang_to_html l)
+    else printf "<li><a href=\"%s\">%s</a></li>\n" fn (lang_to_html l) in
+  List.iter to_html t
+
+let print_hreflang t =
+  let to_link (l, fn, _) =
+    printf "<link rel=\"alternate\" hreflang=%S href=\"%s\" />\n"
+           l (Netencoding.Url.encode fn) in
+  List.iter to_link t
 
 
 let () =
   (* Pass a filename relative to the root of the project.  Example:
      "site/learn/companies.md" *)
-  let path_filename = Sys.argv.(1) in
-  print_endline (translations path_filename)
+  let path_filename = ref "" in
+  let hreflang = ref false in
+  let specs =
+    Arg.align [
+        "--hreflang", Arg.Set hreflang,
+        " output <link> to indicate translations in the header";
+      ] in
+  let msg = "translations <filename>" in
+  Arg.parse specs (fun f -> path_filename := f) msg;
+  if !path_filename = "" then
+    eprintf "You must provide a filename. More info: \"translations -h\".\n";
+  let t = translations !path_filename in
+  if !hreflang then
+    print_hreflang t
+  else
+     print_html t
 
 
 (* Local Variables: *)
