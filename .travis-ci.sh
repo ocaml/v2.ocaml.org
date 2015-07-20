8149a22b1df0a54338e89dc9248a4ee1781ff056
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2015-07-20 17:35:52 +0200

    Turn on caching on Travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,6 +1,14 @@
 
 export OPAMYES=1
-opam init
+
+# $HOME/.opam is cached, hence always present.
+if [ -f "$HOME/.opam/config" ]; then
+    opam update
+    opam upgrade --yes
+else
+    opam init
+fi
+
 if [ -n "${OPAM_SWITCH}" ]; then
     opam switch ${OPAM_SWITCH}
 fi
