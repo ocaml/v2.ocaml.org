2eef248a7bd87bb6ba9ddcb9285ecd9ad7794096
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2015-08-01 10:56:59 +0200

    Travis: reinstall opam from scratch in case of problems

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,8 +3,13 @@ export OPAMYES=1
 
 # $HOME/.opam is cached, hence always present.
 if [ -f "$HOME/.opam/config" ]; then
-    opam update
-    opam upgrade --yes
+    opam update || UPDATE_FAILED="yes"
+    opam upgrade || UPGRADE_FAILED="yes"
+    if [ -n "$UPDATE_FAILED"  -o  -n "$UPGRADE_FAILED" ]; then
+        # Something went wrong, restart from scratch
+        rm -rf "$HOME/.opam/"
+        opam init
+    fi
 else
     opam init
 fi
