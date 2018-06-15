dae62ba528a5cfa91b635c802782cf4b033e1d5c
Léo Andrès <leo@ndrs.fr>
2018-06-15 23:46:09 +0100

    Improve scripts (#1004)

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+
+set -ex
 
 export OPAMYES=1
 
@@ -5,7 +8,7 @@ export OPAMYES=1
 if [ -f "$HOME/.opam/config" ]; then
     opam update || UPDATE_FAILED="yes"
     opam upgrade || UPGRADE_FAILED="yes"
-    if [ -n "$UPDATE_FAILED"  -o  -n "$UPGRADE_FAILED" ]; then
+    if [ -n "$UPDATE_FAILED" ] || [ -n "$UPGRADE_FAILED" ]; then
         # Something went wrong, restart from scratch
         rm -rf "$HOME/.opam/"
         opam init
@@ -15,15 +18,15 @@ else
 fi
 
 if [ -n "${OPAM_SWITCH}" ]; then
-    opam switch ${OPAM_SWITCH}
+    opam switch "$OPAM_SWITCH"
 fi
-eval `opam config env`
+eval "$(opam config env)"
 
 make deps
 RC=$?
 if [ $RC -ne 0 ]; then
     # Try again after updating the environment (bin path)
-    eval `opam config env`
+    eval "$(opam config env)"
     make deps
 fi
 
--- opam.ocaml.org/link_blog_doc_ext.sh
+++ opam.ocaml.org/link_blog_doc_ext.sh
@@ -1,9 +1,11 @@
-#!/bin/sh -ex
+#!/bin/sh
+
+set -ex
 
 BLOG=$1
 DOC=$2
 EXT=$3
 
-ln -n -s -f $BLOG blog
-ln -n -s -f $DOC doc
-ln -n -s -f $EXT ext
+ln -n -s -f "$BLOG" blog
+ln -n -s -f "$DOC" doc
+ln -n -s -f "$EXT" ext
