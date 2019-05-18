1c5d9e2a08f0b234a05e179f76b2797ff24c56e1
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2019-05-18 17:47:17 +0200

    Generate the list of recent OPAM packages from json export
    See https://github.com/ocaml/opam2web/pull/171

--- script/generate_opam_update_list.ml
+++ script/generate_opam_update_list.ml
@@ -1,63 +1,70 @@
 module H = Cow.Html
 
-(* Does not work with opam 2.  See https://github.com/ocaml/opam2web/pull/171 *)
+(* See https://github.com/ocaml/opam2web/pull/171 *)
 
-let packages_base = "https://opam.ocaml.org/packages/"
+let packages_base = "https://opam.ocaml.org/packages"
 
 let staging = try ignore(Sys.getenv "SET_STAGING"); true
               with _ -> false
 
-let top_packages () =
-  let u = O2wUniverse.of_repositories ~preds:[] OpamfUniverse.Index_all
-                                      [`opam] in
-  let dates_fn pkg =
-    try OpamPackage.Map.find pkg u.OpamfUniverse.pkgs_dates
-    with Not_found -> 0. in
-  O2wStatistics.top_packages
-    ~reverse:true ~ntop:6 dates_fn u.OpamfUniverse.max_packages
+let rec take n = function
+  | [] -> []
+  | x :: tl -> if n <= 0 then [] else x :: take (n - 1) tl
 
-let todo () =
-  let to_row (pkg, update_tm) =
-    let open OpamPackage in
-    let name = name pkg in
-    let version = version pkg in
-    let pkg_name = Name.to_string name in
-    let pkg_version = Version.to_string version in
-    let pkg_href =
-      OpamfUniverse.Pkg.href ~href_base:Uri.(of_string packages_base)
-        name version in
-    let pkg_date = O2wMisc.string_of_timestamp update_tm in
-    H.tag "tr"
-      (H.list [H.tag "td" (H.a ~href:pkg_href (H.string pkg_name));
-               H.tag "td" (H.a ~href:pkg_href (H.string pkg_version));
-               H.tag "td" (H.string pkg_date)])
-  in
+(* Get the string associated to the key in an associative JSON list. *)
+let get_string key l =
+  match List.assoc key l with
+  | `String s -> s
+  | _ -> raise Not_found
 
-  let opam_update_list = open_out "opam_update_list" in
-  try
-    let rows = List.map to_row (top_packages ()) in
-    let ch = `Channel opam_update_list in
-    let xml_out = Cow.Xml.make_output ~decl:false ch in
-    List.iter (fun row ->
-        List.iter (Cow.Xml.output xml_out)
-          (`Dtd None :: Template.serialize row)) rows;
-    close_out opam_update_list
-  with e ->
-    if staging then (
-      Printf.fprintf opam_update_list "<tr><td colspan=\"3\">%s %b</td></tr>"
-        (Printexc.to_string e) staging;
-      close_out opam_update_list
-    )
-    else (
-      close_out opam_update_list;
-      raise e
-    )
+let last10 =
+  let fh = open_in "last10_updates.json" in
+  let json = Cow.Json.value_from_channel fh in
+  close_in fh;
+  match json with
+  | `A pkgs ->
+     let add_pkg l = function
+       | `O pkg ->
+          let package = get_string "package" pkg in
+          let pkg_date = get_string "date" pkg in
+          (match String.index_opt package '.' with
+           | Some i ->
+              let j = i + 1 in
+              let pkg_version =
+                if j < String.length package then
+                  String.sub package j (String.length package - j)
+                else "" in
+              (String.sub package 0 i, pkg_version, pkg_date) :: l
+           | None -> (package, "", pkg_date) :: l)
+       | _ -> l in
+     List.rev(List.fold_left add_pkg [] pkgs)
+  | _ -> []
 
 let () =
+  let to_row (pkg_name, pkg_version, pkg_date) =
+    let pkg_href = packages_base ^ "/" ^ pkg_name in
+    let ver_href = pkg_href ^ "/" ^ pkg_name ^ "." ^ pkg_version in
+    let cols = [
+        H.tag "td" (H.a ~href:(Uri.of_string pkg_href) (H.string pkg_name));
+        H.tag "td" (H.a ~href:(Uri.of_string ver_href) (H.string pkg_version));
+        H.tag "td" (H.string pkg_date)] in
+    H.tag "tr" (H.list cols)
+  in
   let opam_update_list = open_out "opam_update_list" in
-  Printf.fprintf opam_update_list
-    "<tr><td colspan=\"3\">The script is being updated</td></tr>";
-  close_out opam_update_list;
+  try
+    let rows = H.list(List.map to_row (take 6 last10)) in
+    H.output (`Channel opam_update_list) rows;
+    close_out opam_update_list
+  with e ->
+    close_out opam_update_list;
+    if staging then (
+      let err = H.tag ~attrs:["colspan", "3"] "td"
+                  (H.list [H.string (Printexc.to_string e);
+                           H.string " (staging)"]) in
+      H.output (`Channel opam_update_list) (H.tag "tr" err);
+    )
+    else
+      raise e
 
 ;;
 (* Local Variables: *)
